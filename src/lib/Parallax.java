064ccff7323e353b0b59cd0e70003cdfff2d6095
IMP1 <IMP1@mail.com>
2015-04-01 12:33:33 +0100

    added auto scrolling

--- src/lib/Parallax.java
+++ src/lib/Parallax.java
@@ -2,10 +2,14 @@ package lib;
 
 public class Parallax {
 	
+	private final int VIEWPORT_WIDTH = jog.Window.getWidth();
+	private final int VIEWPORT_HEIGHT = jog.Window.getHeight();
+	
 	private jog.Image image;
 	private double distance;
 	private double x, y;
 	private boolean scrollHorizontal, scrollVertical;
+	private double autoScrollX, autoScrollY;
 
 	public Parallax(jog.Image image, int x, int y, double distance, boolean scrollHorizontal, boolean scrollVertical) {
 		this.image = image;
@@ -14,6 +18,27 @@ public class Parallax {
 		this.y = y;
 		this.scrollHorizontal = scrollHorizontal;
 		this.scrollVertical = scrollVertical;
+		this.autoScrollX = 0;
+		this.autoScrollY = 0;
+	}
+	
+	public void setAutoScroll(double dx, double dy) {
+		autoScrollX = dx;
+		autoScrollY = dy;
+	}
+	
+	public double getAutoScrollX() {
+		return autoScrollX;
+	}
+	
+	public double getAutoScrollY() {
+		return autoScrollY;
+	}
+	
+	public void update(double dt) {
+		if (autoScrollX != 0 || autoScrollY != 0) {
+			scroll(autoScrollX * dt, autoScrollY * dt);
+		}
 	}
 	
 	public void scroll(double dx, double dy) {
@@ -30,15 +55,15 @@ public class Parallax {
 				if (y > 0) {
 					jog.Graphics.draw(image, x - image.getWidth(), y - image.getHeight());
 				}
-				if (y + image.getHeight() < jog.Window.getHeight()) {
+				if (y + image.getHeight() < VIEWPORT_HEIGHT) {
 					jog.Graphics.draw(image, x - image.getWidth(), y + image.getHeight());
 				}
 			}
-			if (x + image.getWidth() < jog.Window.getWidth()) {
+			if (x + image.getWidth() < VIEWPORT_WIDTH) {
 				if (y > 0) {
 					jog.Graphics.draw(image, x + image.getWidth(), y - image.getHeight());
 				}
-				if (y + image.getHeight() < jog.Window.getHeight()) {
+				if (y + image.getHeight() < VIEWPORT_HEIGHT) {
 					jog.Graphics.draw(image, x + image.getWidth(), y + image.getHeight());
 				}
 			}
@@ -47,7 +72,7 @@ public class Parallax {
 			if (x > 0) {
 				jog.Graphics.draw(image, x - image.getWidth(), y);
 			}
-			if (x + image.getWidth() < jog.Window.getWidth()) {
+			if (x + image.getWidth() < VIEWPORT_WIDTH) {
 				jog.Graphics.draw(image, x + image.getWidth(), y);
 			}
 		}
@@ -55,7 +80,7 @@ public class Parallax {
 			if (y > 0) {
 				jog.Graphics.draw(image, x, y - image.getHeight());
 			}
-			if (y + image.getHeight() < jog.Window.getHeight()) {
+			if (y + image.getHeight() < VIEWPORT_HEIGHT) {
 				jog.Graphics.draw(image, x, y + image.getHeight());
 			}
 		}
