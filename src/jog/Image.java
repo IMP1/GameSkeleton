0da53792261365153e49280b32ab9e16a52ab6f3
IMP1 <IMP1@mail.com>
2016-02-24 18:46:22 +0000

    you can make empty images of sizes and copy/paste image data and set pixels

--- src/jog/Image.java
+++ src/jog/Image.java
@@ -1,5 +1,6 @@
 package jog;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -23,8 +24,30 @@ public class Image extends Graphics.Drawable {
 			System.out.println("[Image] " + path + " loaded.");
 	}
 	
+	public Image(int width, int height) {
+		super(new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB));
+	}
+	
 	private Image(BufferedImage img) {
 		super(img);
+	}
+	
+	public void setPixel(int x, int y, int r, int g, int b) {
+		setPixel(x, y, r, g, b, 255);
+	}
+	public void setPixel(int x, int y, int r, int g, int b, int a) {
+		setPixel(x, y, new Color(r, g, b, a));
+	}
+	public void setPixel(int x, int y, Color colour) {
+		image.setRGB(x, y, colour.getRGB());
+	}
+	
+	public Image copy(int x, int y, int w, int h) {
+		return new Image(image.getSubimage(x, y, w, h));
+	}
+	
+	public void paste(Image img, int x, int y) {
+		image.getGraphics().drawImage(img.image, x, y, null);
 	}
 
 }
